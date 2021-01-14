c14b9fd5b314d744eb5c2de153dfe390c4456f64
aash73 <aash73@gmail.com>
2021-01-14 09:40:20 -0800

    Added dynamically calculating package path

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,6 +19,27 @@ checkVar()
   done
 }
 
+###########################################################
+#This method generates the full path for the conda package given the arguments.
+#Conda Package Format: VersionName + - + VersionNumber + - + "py_" + BuildNnumber + ".tar.bz2"
+#Full path to package: CondaPackageDir + / + CondaPackageName
+#arg1 metafile path
+#arg2 build number
+#arg3 conda package output dir
+generate_conda_package_path() {
+  echo "1st Arg =  " $1
+  echo "2nd Arg =  " $2
+  echo "3rd Arg =  " $3
+  v_name=$(awk '/{% set name =/ {print $5}' $1)
+  v_number=$(awk '/{% set version =/ {print $5}' $1)
+  v_name=$(eval echo $v_name) && v_number=$(eval echo $v_number)
+  #echo "Version Name =  " $v_name
+  #echo "Version Number =  " $v_number
+  package_path="${3}/${v_name}-${v_number}-py_${2}.tar.bz2"
+  #echo "Package Path = " $package_path
+  return 0
+}
+
 #####################################
 #### MAIN 		
 #####################################
@@ -67,8 +88,17 @@ fi
 #conda build -t --output-folder /Users/asimp/conda-examples/conda-pipeline/condabuild/ ./spaghetti-feedstock/recipe/
 #conda build -t condabuild/noarch/spaghetti-1.5.6-py_0.tar.bz2
 
+echo "[C-INFO] Generating final package path ... "
+generate_conda_package_path spaghetti-feedstock/recipe/meta.yaml $BUILD_NUMBER $output_fldr
+if [ $? -eq 0 ]; then
+  echo "[C-INFO] Package Path = $package_path"
+else
+  echo "[C-ERROR] generate_conda_package_path() failed "
+  exit 1
+fi
+
 echo "[C-INFO] uploading conda package to Artifactory ... "
-jfrog rt u $output_fldr/noarch/spaghetti-1.5.6-py_0.tar.bz2 $target_repo/ --build-name=$build_id --build-number=$build_number
+jfrog rt u $package_path $target_repo/ --build-name=$build_id --build-number=$build_number
 
 if [ $? -eq 0 ]; then 
   echo -e "\n[C-INFO] conda package uploaded !"
